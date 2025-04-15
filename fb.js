a9760362095e057c00c23555f5231929ea3e99ba
Mohamed Suhaib Nadakkavil <suhaibmd268@gmail.com>
2025-04-15 07:46:33 +0530

    Add files via upload

---
+++ fb.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#sighup-form").validate({
+        rules:{
+            fname:{
+                required:true,
+            },
+            sname:{
+                
+            }
+
+            }
+
+        })
+       
+    })
